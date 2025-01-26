d4bee4a42d100db2c85ef81f20e8eb495cf86329
Laura Caroline <laura@laura-melo.com>
2025-01-26 14:58:04 +0100

    chore: generate types from database

---
+++ types/database.types.ts
@@ -0,0 +1,386 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export type Database = {
+  public: {
+    Tables: {
+      actions: {
+        Row: {
+          action_category: Database["public"]["Enums"]["Action Categories"]
+          action_result: Database["public"]["Enums"]["Action Results"]
+          created_at: string
+          id: number
+          opposite_team_action: boolean | null
+          player_id: number | null
+          set_id: number
+        }
+        Insert: {
+          action_category: Database["public"]["Enums"]["Action Categories"]
+          action_result: Database["public"]["Enums"]["Action Results"]
+          created_at?: string
+          id?: number
+          opposite_team_action?: boolean | null
+          player_id?: number | null
+          set_id: number
+        }
+        Update: {
+          action_category?: Database["public"]["Enums"]["Action Categories"]
+          action_result?: Database["public"]["Enums"]["Action Results"]
+          created_at?: string
+          id?: number
+          opposite_team_action?: boolean | null
+          player_id?: number | null
+          set_id?: number
+        }
+        Relationships: [
+          {
+            foreignKeyName: "actions_player_id_fkey"
+            columns: ["player_id"]
+            isOneToOne: false
+            referencedRelation: "team_members"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "actions_set_id_fkey"
+            columns: ["set_id"]
+            isOneToOne: false
+            referencedRelation: "sets"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      games: {
+        Row: {
+          created_at: string
+          date: string
+          home_game: boolean
+          id: number
+          opponent_team_name: string
+          team_id: number
+        }
+        Insert: {
+          created_at?: string
+          date: string
+          home_game: boolean
+          id?: number
+          opponent_team_name: string
+          team_id: number
+        }
+        Update: {
+          created_at?: string
+          date?: string
+          home_game?: boolean
+          id?: number
+          opponent_team_name?: string
+          team_id?: number
+        }
+        Relationships: [
+          {
+            foreignKeyName: "Games_team_id_fkey"
+            columns: ["team_id"]
+            isOneToOne: false
+            referencedRelation: "teams"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      sets: {
+        Row: {
+          created_at: string
+          game_id: number
+          id: number
+          starting_player_1: number
+          starting_player_2: number
+          starting_player_3: number
+          starting_player_4: number
+          starting_player_5: number
+          starting_player_6: number
+          team_starts_serving: boolean
+        }
+        Insert: {
+          created_at?: string
+          game_id: number
+          id?: number
+          starting_player_1: number
+          starting_player_2: number
+          starting_player_3: number
+          starting_player_4: number
+          starting_player_5: number
+          starting_player_6: number
+          team_starts_serving: boolean
+        }
+        Update: {
+          created_at?: string
+          game_id?: number
+          id?: number
+          starting_player_1?: number
+          starting_player_2?: number
+          starting_player_3?: number
+          starting_player_4?: number
+          starting_player_5?: number
+          starting_player_6?: number
+          team_starts_serving?: boolean
+        }
+        Relationships: [
+          {
+            foreignKeyName: "sets_game_id_fkey"
+            columns: ["game_id"]
+            isOneToOne: false
+            referencedRelation: "games"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "sets_starting_player_1_fkey"
+            columns: ["starting_player_1"]
+            isOneToOne: false
+            referencedRelation: "team_members"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "sets_starting_player_2_fkey"
+            columns: ["starting_player_2"]
+            isOneToOne: false
+            referencedRelation: "team_members"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "sets_starting_player_3_fkey"
+            columns: ["starting_player_3"]
+            isOneToOne: false
+            referencedRelation: "team_members"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "sets_starting_player_4_fkey"
+            columns: ["starting_player_4"]
+            isOneToOne: false
+            referencedRelation: "team_members"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "sets_starting_player_5_fkey"
+            columns: ["starting_player_5"]
+            isOneToOne: false
+            referencedRelation: "team_members"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "sets_starting_player_6_fkey"
+            columns: ["starting_player_6"]
+            isOneToOne: false
+            referencedRelation: "team_members"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      team_members: {
+        Row: {
+          access: Database["public"]["Enums"]["Access Type"]
+          created_at: string
+          id: number
+          name: string
+          team_id: number
+          uniform_numbers: number[] | null
+          user_id: string | null
+        }
+        Insert: {
+          access?: Database["public"]["Enums"]["Access Type"]
+          created_at?: string
+          id?: number
+          name: string
+          team_id: number
+          uniform_numbers?: number[] | null
+          user_id?: string | null
+        }
+        Update: {
+          access?: Database["public"]["Enums"]["Access Type"]
+          created_at?: string
+          id?: number
+          name?: string
+          team_id?: number
+          uniform_numbers?: number[] | null
+          user_id?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "team_members_team_id_fkey"
+            columns: ["team_id"]
+            isOneToOne: false
+            referencedRelation: "teams"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      teams: {
+        Row: {
+          created_at: string
+          id: number
+          logo_url: string | null
+          name: string
+        }
+        Insert: {
+          created_at?: string
+          id?: number
+          logo_url?: string | null
+          name: string
+        }
+        Update: {
+          created_at?: string
+          id?: number
+          logo_url?: string | null
+          name?: string
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      "Access Type": "admin" | "statistics" | "read_only"
+      "Action Attack Results":
+        | "direct_point"
+        | "touch_point"
+        | "out"
+        | "in_game"
+        | "into_the_net"
+        | "blocked"
+      "Action Block Results": "direct_point" | "touch_point" | "in_game" | "out"
+      "Action Categories": "serve" | "pass" | "set" | "attack" | "block" | "dig"
+      "Action Dig Results": "good" | "bad" | "miss"
+      "Action Pass Results": "good" | "bad" | "miss"
+      "Action Results":
+        | "direct_point"
+        | "touch_point"
+        | "in_game"
+        | "out"
+        | "into_the_net"
+        | "blocked"
+        | "good"
+        | "bad"
+        | "miss"
+      "Action Serve Results":
+        | "direct_point"
+        | "touch_point"
+        | "out"
+        | "into_the_net"
+        | "in_game"
+      "Action Set Results": "good" | "bad" | "miss"
+      "Player Positions":
+        | "coach"
+        | "libero"
+        | "setter"
+        | "middle_blocker"
+        | "opposite_hitter"
+        | "outside_hitter"
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+type PublicSchema = Database[Extract<keyof Database, "public">]
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
+        PublicSchema["Views"])
+    ? (PublicSchema["Tables"] &
+        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
+    : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
+    : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
+    : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof PublicSchema["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof PublicSchema["CompositeTypes"]
+    | { schema: keyof Database },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
+    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never
