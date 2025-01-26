391af50ed07e7f27c88822b28df4f4ef331b8ab6
Laura Caroline <laura@laura-melo.com>
2025-01-26 19:35:04 +0100

    chore: add normalized enum types

---
+++ types/database.enums.ts
@@ -0,0 +1,12 @@
+import type { Database } from "~/types/database.types";
+
+export type AccessType = Database["public"]["Enums"]["Access Type"];
+export type PlayerPositions = Database["public"]["Enums"]["Player Positions"];
+export type ActionCategories = Database["public"]["Enums"]["Action Categories"];
+
+export type ActionAttackResults = Database["public"]["Enums"]["Action Attack Results"];
+export type ActionBlockResults = Database["public"]["Enums"]["Action Block Results"];
+export type ActionDigResults = Database["public"]["Enums"]["Action Dig Results"];
+export type ActionPassResults = Database["public"]["Enums"]["Action Pass Results"];
+export type ActionServeResults = Database["public"]["Enums"]["Action Serve Results"];
+export type ActionSetResults = Database["public"]["Enums"]["Action Set Results"];
